7845583d854348693150978703c2f7f4eb909a7d
xinzheshen <xinzheshen@163.com>
2019-06-20 23:20:37 +0800

    flask

--- src/web/flask/app.py
+++ src/web/flask/app.py
@@ -1,7 +1,37 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template
 from flask import request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# 数据库配置：数据库地址，关闭自动跟踪修改
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@127.0.0.1:3306/flask_books'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#创建数据库对象
+db = SQLAlchemy(app)
+'''
+1 配置数据库
+2 添加书和作者模型
+3 添加数据
+4 使用模板显示数据库查询结果
+5 使用WTF显示表单
+6 实现相关的增删逻辑
+'''
+
+# 定义书和作者模型
+# 作者模型
+class Author(db.Model):
+    # 表名
+    __tablename__ = 'authors'
+    # 字段
+    id = db.Column(db.Interger, primary_key=True)
+
+
+
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
